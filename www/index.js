dcb6041819957338f81ca1ee357f371008e6f3e8
Emmanuel Delaborde <manu.delab@gmail.com>
2019-06-11 19:01:11 +1200

    draw the cell, alternate play

--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
-import { Board, Cell } from "connect4";
+import { Board, Cell, Move } from "connect4";
 import { memory } from "connect4/connect4_bg";
 
 const CELL_SIZE = 60; // px
@@ -12,19 +12,13 @@ const board = Board.new();
 const width = board.width();
 const height = board.height();
 
+const msg = document.getElementById("msg");
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
 const canvas = document.getElementById("canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
-
 const ctx = canvas.getContext('2d');
-
-const renderLoop = () => {
-  drawGrid();
-  drawCells();
-  //requestAnimationFrame(renderLoop);
-};
 
 const drawGrid = () => {
   ctx.beginPath();
@@ -51,7 +45,6 @@ const drawCells = () => {
         : cells[idx] === Cell.Yellow
           ? YELLOW_COLOR
           : RED_COLOR;
-
       let x = col * CELL_SIZE + CELL_SIZE / 2;
       let y = row * CELL_SIZE + CELL_SIZE / 2;
       ctx.arc(x, y, CELL_SIZE / 2 - 2, 0, 2 * Math.PI);
@@ -66,8 +59,48 @@ function getCursorPosition(canvas, event) {
   var x = event.clientX - rect.left;
   var y = event.clientY - rect.top;
   console.log("x: " + x + " y: " + y);
+  return [x, y];
 }
 
-canvas.onclick = (e) => getCursorPosition(canvas, e);
+const showPlayer = () => {
+  return player === Cell.Yellow ? "Yellow" : "Red";
+}
 
-renderLoop();
+const render = () => {
+  msg.innerHTML = showPlayer() + " turn to play";
+  drawGrid();
+  drawCells();
+};
+
+const swapPlayer = (p) => {
+  return p === Cell.Yellow ? Cell.Red : Cell.Yellow;
+}
+
+
+const handleClick = (e) => {
+  let [x, y] = getCursorPosition(canvas, e);
+  // here translate x,y to a cell 
+  let row = Math.floor(y / CELL_SIZE);
+  let column = Math.floor(x / CELL_SIZE);
+  let idx = (row * width + column);
+  // send move
+  let move = board.play(player, idx);
+  if (move == Move.Ok) {
+    // render the board + cells
+    render();
+    // is game finished? announce winner, deregister click handler
+
+    // if not let the other play
+    player = swapPlayer(player);
+  }
+  else {
+    console.log('Invalid move')
+  }
+
+
+}
+
+// Start the game
+let player = Cell.Yellow;
+canvas.onclick = (e) => handleClick(e);
+render();
